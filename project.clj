cf692bc3f6fba0314250dd9f8ee8b197f73fc83c
Andy Marks <amarks@thoughtworks.com>
2015-08-15 10:52:07 +1000

    No longer using beatr because of freesound api issues

--- project.clj
+++ project.clj
@@ -5,6 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main tutorial.core
   :dependencies [[org.clojure/clojure "1.6.0"]
-  				 [beatr.beatr "0.1.0-SNAPSHOT"]
-  				 [quil "1.6.0" :exclusions [org.clojure/clojure]]
                  [overtone "0.9.1"]])
