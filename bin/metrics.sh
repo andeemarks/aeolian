7e8fa60fd6fc037be668e0f16cef3fc4d5337490
Andy Marks <amarks@thoughtworks.com>
2015-08-21 21:55:21 +1000

    Updated script to take source file as parameter

--- bin/metrics.sh
+++ bin/metrics.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-CHECKSTYLE_JAR=/path/to/checkstyle.jar
+CHECKSTYLE_JAR=../resources/checkstyle-6.9-all.jar
 CHECKSTYLE_PROPS=../resources/checkstyle.xml
 
 # Generate some complexity metrics from Checkstyle
-java -jar $CHECKSTYLE_JAR -c $CHECKSTYLE_PROPS Foo.java | grep "Cyclomatic Complexity" | awk '{print $1 " " $5}' | awk -F: '{print $2 " " $4}' > complexity.txt
+java -jar $CHECKSTYLE_JAR -c $CHECKSTYLE_PROPS $1 | grep "Cyclomatic Complexity" | awk '{print $1 " " $5}' | awk -F: '{print $2 " " $4}' > complexity.txt
 
 # Generate some line length metrics from awk
-cat Foo.java | awk '{print NR " " length($0)}' > line-lengths.txt
+cat $1 | awk '{print NR " " length($0)}' > line-lengths.txt
 
 # Merge the two datasets
-join -a1 <(sort line-lengths.txt) <(sort complexity.txt) > combined-metrics.txt
+join -a1 <(sort line-lengths.txt) <(sort complexity.txt) | sort -n > combined-metrics.txt
