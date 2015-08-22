9cd0de09998243a1a3eb3f726cb4dab34576daa6
Andy Marks <amarks@thoughtworks.com>
2015-08-22 20:25:14 +1000

    Now abending with file argument

--- bin/metrics.sh
+++ bin/metrics.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ "$#" -ne 1 ]
+then
+  echo "Usage: metrics.sh <Java source file>"
+  exit 1
+fi
+
 CHECKSTYLE_JAR=../resources/checkstyle-6.9-all.jar
 CHECKSTYLE_PROPS=../resources/checkstyle.xml
 
@@ -9,13 +15,14 @@ TEMP_LENGTH_METRICS=`mktemp /tmp/line-lengths.txt.XXXXXX` || exit 1
 
 COMBINED_METRICS=combined-metrics.txt
 
-# Generate some complexity metrics from Checkstyle
+echo "Processing $1..."
+echo "1. Generating complexity metrics with Checkstyle..."
 java -jar $CHECKSTYLE_JAR -c $CHECKSTYLE_PROPS $1 | grep "Cyclomatic Complexity" | awk '{print $1 " " $5}' | awk -F: '{print $2 " " $4}' > $TEMP_COMPLEXITY_METRICS
 
-# Generate some line length metrics from awk
+echo "2. Generating line length metrics with awk..."
 cat $1 | awk '{print NR " " length($0)}' > $TEMP_LENGTH_METRICS
 
-# Merge the two datasets
+echo "3. Merging metric datasets..."
 join -a1 <(sort $TEMP_LENGTH_METRICS) <(sort $TEMP_COMPLEXITY_METRICS) | sort -n > $COMBINED_METRICS
 
-echo "Generating " $COMBINED_METRICS
+echo "Generated " $COMBINED_METRICS
