c75f2937beb8fb7568ceb8605b1d69620e8f3dca
Andy Marks <amarks@thoughtworks.com>
2015-08-22 20:27:40 +1000

    Now checking if argument is valid file ref

--- bin/metrics.sh
+++ bin/metrics.sh
@@ -6,6 +6,13 @@ then
   exit 1
 fi
 
+SOURCE_FILE=$1
+
+if [ ! -f $SOURCE_FILE ]; then
+  echo "Usage: metrics.sh <Java source file>"
+  exit 1
+fi
+
 CHECKSTYLE_JAR=../resources/checkstyle-6.9-all.jar
 CHECKSTYLE_PROPS=../resources/checkstyle.xml
 
@@ -15,12 +22,12 @@ TEMP_LENGTH_METRICS=`mktemp /tmp/line-lengths.txt.XXXXXX` || exit 1
 
 COMBINED_METRICS=combined-metrics.txt
 
-echo "Processing $1..."
+echo "Processing $SOURCE_FILE..."
 echo "1. Generating complexity metrics with Checkstyle..."
-java -jar $CHECKSTYLE_JAR -c $CHECKSTYLE_PROPS $1 | grep "Cyclomatic Complexity" | awk '{print $1 " " $5}' | awk -F: '{print $2 " " $4}' > $TEMP_COMPLEXITY_METRICS
+java -jar $CHECKSTYLE_JAR -c $CHECKSTYLE_PROPS $SOURCE_FILE | grep "Cyclomatic Complexity" | awk '{print $1 " " $5}' | awk -F: '{print $2 " " $4}' > $TEMP_COMPLEXITY_METRICS
 
 echo "2. Generating line length metrics with awk..."
-cat $1 | awk '{print NR " " length($0)}' > $TEMP_LENGTH_METRICS
+cat $SOURCE_FILE | awk '{print NR " " length($0)}' > $TEMP_LENGTH_METRICS
 
 echo "3. Merging metric datasets..."
 join -a1 <(sort $TEMP_LENGTH_METRICS) <(sort $TEMP_COMPLEXITY_METRICS) | sort -n > $COMBINED_METRICS
