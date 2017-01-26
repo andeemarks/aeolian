73168141c64f0db1922c7947e2d9eeffb3974be3
Andy Marks <amarks@thoughtworks.com>
2017-01-26 21:08:31 +1100

    Naming

--- resources/go.sh
+++ resources/go.sh
@@ -5,14 +5,15 @@ if [[ $# -eq 0 ]]; then
     exit 1
 fi
 
-if [[ -r $1 ]]; then
+SOURCEFILE=$1
+if [[ -r $SOURCEFILE ]]; then
 	echo "Generating some complexity metrics from Checkstyle..."
-	java -jar checkstyle-7.4-all.jar -c checkstyle.xml $1 | grep "Cyclomatic Complexity" | awk '{print $2 " " $6}' | awk -F: '{print $2 " " $4}' > complexity.txt
+	java -jar checkstyle-7.4-all.jar -c checkstyle.xml $SOURCEFILE | grep "Cyclomatic Complexity" | awk '{print $2 " " $6}' | awk -F: '{print $2 " " $4}' > complexity.txt
 	echo "Generating some line length metrics from awk..."
-	cat $1 | awk '{print NR " " length($0)}' > line-lengths.txt
+	cat $SOURCEFILE | awk '{print NR " " length($0)}' > line-lengths.txt
 	echo "Merging the two datasets..."
 	join -a1 <(sort line-lengths.txt) <(sort complexity.txt) > combined-metrics.txt
 else
-    >&2 echo "$1 does not exist or cannot be read"
+    >&2 echo "Error: $SOURCEFILE does not exist or cannot be read"
     exit 1
 fi
