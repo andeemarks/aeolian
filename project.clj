d195d26bcc88bc0ca4a2a6c7f68b61ce795aeb1f
Andy Marks <amarks@thoughtworks.com>
2017-01-26 20:32:08 +1100

    Added sample intermediate metric files

--- project.clj
+++ project.clj
@@ -5,5 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main aeolian.core
   :jvm-opts ^:replace []
-  :dependencies [	[org.clojure/clojure "1.8.0"]
-  					[midje "1.8.3"]])
+  :dependencies [	[org.clojure/clojure "1.8.0"]]
+  :profiles 
+  	{:dev 
+  		{:dependencies 
+  			[
+  				[midje "1.8.3" :exclusions [org.clojure/clojure]]]}})
