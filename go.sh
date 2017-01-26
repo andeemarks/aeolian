5a2306259388fc9db3ce0ff287caeefe45958b17
Andy Marks <amarks@thoughtworks.com>
2017-01-26 21:33:01 +1100

    go script now top level

--- go.sh
+++ go.sh
@@ -5,10 +5,21 @@ if [[ $# -eq 0 ]]; then
     exit 1
 fi
 
+if ! which abc2midi &> /dev/null; then
+	>&2 echo “Error: Please install abc2midi and make sure it is in your path”
+	exit 1
+fi
+
+if ! which timidity &> /dev/null; then
+	>&2 echo “Error: Please install timidity and make sure it is in your path”
+	exit 1
+fi
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SOURCEFILE=$1
 if [[ -r $SOURCEFILE ]]; then
 	echo "Generating some complexity metrics from Checkstyle..."
-	java -jar checkstyle-7.4-all.jar -c checkstyle.xml $SOURCEFILE | grep "Cyclomatic Complexity" | awk '{print $2 " " $6}' | awk -F: '{print $2 " " $4}' > /tmp/complexity.txt
+	java -jar $DIR/resources/checkstyle-7.4-all.jar -c $DIR/resources/checkstyle.xml $SOURCEFILE | grep "Cyclomatic Complexity" | awk '{print $2 " " $6}' | awk -F: '{print $2 " " $4}' > /tmp/complexity.txt
 	echo "Generating some line length metrics from awk..."
 	cat $SOURCEFILE | awk '{print NR " " length($0)}' > /tmp/line-lengths.txt
 	echo "Merging the two datasets..."
