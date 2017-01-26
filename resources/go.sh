f9f583220fb39bf510d8010dcc86f85eedeb97f0
Andy Marks <amarks@thoughtworks.com>
2017-01-26 21:12:19 +1100

    Temp files now going to tmp dir

--- resources/go.sh
+++ resources/go.sh
@@ -8,11 +8,11 @@ fi
 SOURCEFILE=$1
 if [[ -r $SOURCEFILE ]]; then
 	echo "Generating some complexity metrics from Checkstyle..."
-	java -jar checkstyle-7.4-all.jar -c checkstyle.xml $SOURCEFILE | grep "Cyclomatic Complexity" | awk '{print $2 " " $6}' | awk -F: '{print $2 " " $4}' > complexity.txt
+	java -jar checkstyle-7.4-all.jar -c checkstyle.xml $SOURCEFILE | grep "Cyclomatic Complexity" | awk '{print $2 " " $6}' | awk -F: '{print $2 " " $4}' > /tmp/complexity.txt
 	echo "Generating some line length metrics from awk..."
-	cat $SOURCEFILE | awk '{print NR " " length($0)}' > line-lengths.txt
+	cat $SOURCEFILE | awk '{print NR " " length($0)}' > /tmp/line-lengths.txt
 	echo "Merging the two datasets..."
-	join -a1 <(sort line-lengths.txt) <(sort complexity.txt) > combined-metrics.txt
+	join -a1 <(sort /tmp/line-lengths.txt) <(sort /tmp/complexity.txt) > combined-metrics.txt
 else
     >&2 echo "Error: $SOURCEFILE does not exist or cannot be read"
     exit 1
