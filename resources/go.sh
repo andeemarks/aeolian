ebd64deb3fa81ed6a58c8eee314829bd17657829
Andy Marks <amarks@thoughtworks.com>
2017-01-26 21:02:28 +1100

    Aborting without arg

--- resources/go.sh
+++ resources/go.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+if [ $# -eq 0 ]
+  then
+    >&2 echo "Usage: go.sh <source-file.java>"
+    exit -1
+fi
+
 echo "Generating some complexity metrics from Checkstyle..."
 java -jar checkstyle-7.4-all.jar -c checkstyle.xml $1 | grep "Cyclomatic Complexity" | awk '{print $2 " " $6}' | awk -F: '{print $2 " " $4}' > complexity.txt
 echo "Generating some line length metrics from awk..."
