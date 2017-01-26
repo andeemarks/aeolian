344a54602d4c1c7a049288d743920115e4456684
Andy Marks <amarks@thoughtworks.com>
2017-01-26 20:41:58 +1100

    Combined shell preprocessing into basic script

---
+++ resources/go.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Generating some complexity metrics from Checkstyle..."
+java -jar checkstyle-7.4-all.jar -c checkstyle.xml $1 | grep "Cyclomatic Complexity" | awk '{print $2 " " $6}' | awk -F: '{print $2 " " $4}' > complexity.txt
+echo "Generating some line length metrics from awk..."
+cat $1 | awk '{print NR " " length($0)}' > line-lengths.txt
+echo "Merging the two datasets..."
+join -a1 <(sort line-lengths.txt) <(sort complexity.txt) > combined-metrics.txt
