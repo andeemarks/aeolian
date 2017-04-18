9c78280552a12bfacc32c6f25b0dc37bb6172e82
Andy Marks <amarks@thoughtworks.com>
2017-04-19 09:40:40 +1000

    Fixed sort ordering issues for join + added header to combined metrics

--- go.sh
+++ go.sh
@@ -16,16 +16,19 @@ if ! which timidity &> /dev/null; then
 fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CHECKSTYLEDIR=$DIR/resources
 SOURCEFILE=$1
+COMBINEDMETRICSFILE='combined-metrics.txt'
 if [[ -r $SOURCEFILE ]]; then
 	echo -e "\e[33mGenerating Checkstyle cyclomatic complexity metrics...\e[0m"
-	java -jar $DIR/resources/checkstyle-7.4-all.jar -c $DIR/resources/checkstyle-complexity.xml $SOURCEFILE | grep "CyclomaticComplexity" | awk '{print $2 " " $3}' | awk -F: '{print $1 "_" $2 " " $4}' > /tmp/complexity.txt
+	java -jar $CHECKSTYLEDIR/checkstyle-7.4-all.jar -c $CHECKSTYLEDIR/checkstyle-complexity.xml $SOURCEFILE | grep "CyclomaticComplexity" | awk '{print $2 " " $3}' | awk -F: '{print $1 "_" $2 " " $4}' > /tmp/complexity.txt
 	echo -e "\e[33mGenerating Checkstyle line length metrics...\e[0m"
-	java -jar $DIR/resources/checkstyle-7.4-all.jar -c $DIR/resources/checkstyle-linelength.xml $SOURCEFILE | grep "LineLength" | awk '{print $2 " " $3}' | awk -F: '{print $1 "_" $2 " " $3}' > /tmp/line-lengths.txt
+	java -jar $CHECKSTYLEDIR/checkstyle-7.4-all.jar -c $CHECKSTYLEDIR/checkstyle-linelength.xml $SOURCEFILE | grep "LineLength" | awk '{print $2 " " $3}' | awk -F: '{print $1 "_" $2 " " $3}' > /tmp/line-lengths.txt
 	echo -e "\e[33mMerging the two datasets...\e[0m"
-	join -a1 <(sort -g --key=1 /tmp/line-lengths.txt) <(sort -g --key=1 /tmp/complexity.txt) > combined-metrics.txt
+	echo "[source-file_line-number] [line-length] [cyclomatic-complexity]" > $COMBINEDMETRICSFILE
+	join -a 1 <(sort -k 1b,1 /tmp/line-lengths.txt) <(sort -k 1b,1 /tmp/complexity.txt) >> $COMBINEDMETRICSFILE
 	echo -e "\e[33mGenerating ABC notation...\e[0m"
-	lein run combined-metrics.txt
+	lein run $COMBINEDMETRICSFILE
 	echo -e "\e[33mGenerating MIDI...\e[0m"
 	abc2midi combined-metrics.txt.abc
 	echo -e "\e[33mPlaying MIDI...\e[0m"
