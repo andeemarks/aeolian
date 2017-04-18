fc10f8f9d0250ddba6d1cba0b0eca820c97e8209
Andy Marks <amarks@thoughtworks.com>
2017-04-18 15:11:01 +1000

    Fixed parsing for Checkstyle generated line length metrics

--- go.sh
+++ go.sh
@@ -19,9 +19,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SOURCEFILE=$1
 if [[ -r $SOURCEFILE ]]; then
 	echo -e "\e[33mGenerating Checkstyle cyclomatic complexity metrics...\e[0m"
-	java -jar $DIR/resources/checkstyle-7.4-all.jar -c $DIR/resources/checkstyle.xml $SOURCEFILE | grep "CyclomaticComplexity" | awk '{print $2 " " $3}' | awk -F: '{print $1 " " $2 " " $4}' > /tmp/complexity.txt
-	echo -e "\e[33mGenerating Awk line length metrics...\e[0m"
-	awk '{print FILENAME " " NR " " length($0)}' $SOURCEFILE > /tmp/line-lengths.txt
+	java -jar $DIR/resources/checkstyle-7.4-all.jar -c $DIR/resources/checkstyle-complexity.xml $SOURCEFILE | grep "CyclomaticComplexity" | awk '{print $2 " " $3}' | awk -F: '{print $1 " " $2 " " $4}' > /tmp/complexity.txt
+	echo -e "\e[33mGenerating Checkstyle line length metrics...\e[0m"
+	java -jar $DIR/resources/checkstyle-7.4-all.jar -c $DIR/resources/checkstyle-linelength.xml $SOURCEFILE | grep "LineLength" | awk '{print $2 " " $3}' | awk -F: '{print $1 " " $2 " " $3}' > /tmp/line-lengths.txt
 	echo -e "\e[33mMerging the two datasets...\e[0m"
 	join -a1 <(sort -g --key=2 /tmp/line-lengths.txt) <(sort -g --key=2 /tmp/complexity.txt) > combined-metrics.txt
 	echo -e "\e[33mGenerating ABC notation...\e[0m"
