800eaf2de8181f8b4651f5a06a8cc4e959cbc985
Andy Marks <amarks@thoughtworks.com>
2017-04-19 10:39:50 +1000

    More intentional around separation of header from body of metrics structure

--- src/aeolian/composer.clj
+++ src/aeolian/composer.clj
@@ -21,7 +21,9 @@
 			:else note)))
 
 (defn compose [notes-per-measure line-metrics notation-file-name]
-	(let [mapped-notes (map #(metric-to-note %1) (rest line-metrics))
+	(let [header (first line-metrics)
+		metrics (rest line-metrics)
+		mapped-notes (map #(metric-to-note %1) metrics)
 		notes-in-measures (apply str (flatten (interpose (str "|\n" c-major-root) (partition notes-per-measure mapped-notes))))
 		completed-score (str header "|" c-major-root notes-in-measures "|") ]
 		(spit notation-file-name, completed-score) ) )
