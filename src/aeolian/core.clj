9e904e5c8e2047449eef5dad609d9cb1cc07ae3d
Andy Marks <amarks@thoughtworks.com>
2017-04-19 10:30:14 +1000

    Separating File I/O from music generation

--- src/aeolian/core.clj
+++ src/aeolian/core.clj
@@ -1,31 +1,8 @@
 (ns aeolian.core
 	(:require [aeolian.parser :as parser]
 						[aeolian.tempo :as tempo]
+						[aeolian.composer :as composer]
 						[clojure.java.io :as io]))
-
-(def c-major-notes ["C" "E" "G" "B" "z" "c" "e" "g" "b" "z" "c'" "e'" "g'" "b'" "z"])
-(def c-major-chords ["\"C\"" "\"F\"" "\"G\"" "\"Am\""])
-(def c-major-root "\"Am\"")
-(def drum-beat "%%MIDI program 0\n%%MIDI drum zd 60\n%%MIDI drumon\n%%MIDI gchord c")
-(def header (str "X:1\nT:Hello World\nM:4/4\nL:1/4\nK:C\n" drum-beat "\n" tempo/abc-template tempo/default-tempo "\n"))
-
-
-(defn- metric-to-note-index [metric]
-	(mod (parser/line-length-from-metric metric) (count c-major-notes)))
-
-(defn- metric-to-note [metric]
-	(let [complexity (parser/complexity-from-metric metric)
-				note (nth c-major-notes (metric-to-note-index metric))]
-		(cond (> complexity 1)
-			(str "\n" (tempo/as-abc complexity) "\n" note)
-			; note
-			:else note)))
-
-(defn- generate-notation [notes-per-measure line-metrics notation-file-name]
-	(let [mapped-notes (map #(metric-to-note %1) line-metrics)
-		notes-in-measures (apply str (flatten (interpose (str "|\n" c-major-root) (partition notes-per-measure mapped-notes))))
-		completed-score (str header "|" c-major-root notes-in-measures "|") ]
-		(spit notation-file-name, completed-score) ) )
 
 (defn notation-file-name [original-file-name]
 	(str original-file-name ".abc"))
@@ -34,7 +11,7 @@
 	; (println (str "Generating ABC Notation from " metrics-file-name "..."))
 	(with-open [rdr (clojure.java.io/reader metrics-file-name)]
      (let [notation-file-name (notation-file-name metrics-file-name)
-     		_ (generate-notation 4 (line-seq rdr) notation-file-name)]
+     		_ (composer/compose 4 (line-seq rdr) notation-file-name)]
      		(println (str "Generated " notation-file-name))))
 	)
 
