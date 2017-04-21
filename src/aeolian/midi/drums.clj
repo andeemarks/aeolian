5f7c10dc3c928d76c59016755d5dd017a520f60d
Andy Marks <amarks@thoughtworks.com>
2017-04-21 11:08:32 +1000

    Removed beat accents to hear each note better

--- src/aeolian/midi/drums.clj
+++ src/aeolian/midi/drums.clj
@@ -6,6 +6,7 @@
 (def beat stick)
 
 (def four-beat-metro (str "%%MIDI drum dddd " beat " " beat " " beat " " beat " ""\n"))
+(def no-accents "%%MIDI nobeataccents")
 (def first-beat-emphasis "%%MIDI beatstring pmmm\n")
 
-(def drum-track (str "%%MIDI program 32\n" first-beat-emphasis four-beat-metro "%%MIDI drumon\n%%MIDI gchord c\n"))
+(def drum-track (str "%%MIDI program 32\n" first-beat-emphasis four-beat-metro no-accents "%%MIDI drumon\n%%MIDI gchord c\n"))
