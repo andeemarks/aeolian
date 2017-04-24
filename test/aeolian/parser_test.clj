7f411b6e7f8ea424cf338396b6e65de18f490892
Andy Marks <amarks@thoughtworks.com>
2017-04-24 21:10:28 +1000

    Field ordering no longer important with labels

--- test/aeolian/parser_test.clj
+++ test/aeolian/parser_test.clj
@@ -4,28 +4,28 @@
 
 (facts "When parsing a metric line"
   (facts "the line length"
-    (fact "is the second value"
-      (parser/line-length-from-metric "/Notification.java#190 LL=13 CC=9 ML=16") => 13)
+    (fact "can be found"
+      (parser/line-length-from-metric "/Notification.java#190 LL=13") => 13)
 
     (fact "must be numeric"
-      (parser/line-length-from-metric "/Notification.java#190 LL=abc CC=9 ML=16") => (throws Exception)))
+      (parser/line-length-from-metric "/Notification.java#190 LL=abc") => (throws Exception)))
 
   (facts "the complexity"
-    (fact "is the third value"
-      (parser/complexity-from-metric "/Notification.java#190 LL=13 CC=9 ML=16") => 9)
+    (fact "can be found"
+      (parser/complexity-from-metric "/Notification.java#190 CC=9") => 9)
 
     (fact "must be numeric"
-      (parser/complexity-from-metric "/Notification.java#190 LL=13 CC=abc ML=16") => (throws Exception))
+      (parser/complexity-from-metric "/Notification.java#190 CC=abc") => (throws Exception))
 
     (fact "defaults to 0"
       (parser/complexity-from-metric "/Notification.java#190 LL=13") => 0))
 
   (facts "the method length"
-    (fact "is the fourth value"
-      (parser/method-length-from-metric "/Notification.java#190 LL=13 CC=9 ML=16") => 16)
+    (fact "can be found"
+      (parser/method-length-from-metric "/Notification.java#190 ML=16") => 16)
 
     (fact "must be numeric"
-      (parser/method-length-from-metric "/Notification.java#190 LL=13 CC=abc ML=abc") => (throws Exception))
+      (parser/method-length-from-metric "/Notification.java#190 ML=abc") => (throws Exception))
 
     (fact "defaults to 0"
       (parser/method-length-from-metric "/Notification.java#190 LL=13") => 0)
