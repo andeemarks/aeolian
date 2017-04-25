49b4e25ec39960e9afdb84e464c1109f689e11da
Andy Marks <amarks@thoughtworks.com>
2017-04-25 22:07:28 +1000

    Simplification

--- src/aeolian/abc/notes.clj
+++ src/aeolian/abc/notes.clj
@@ -10,22 +10,21 @@
 (def or-root "\"Am\"")
 
 (defn pick-chord-for-method-length [method-length]
-	(if (nil? method-length)
-		"C"
-		(cond
-		   (<= 1 method-length 5) "C"
-		   (<= 6 method-length 10) "D"
-		   (<= 11 method-length 15) "E"
-		   (<= 16 method-length 20) "F"
-		   (<= 21 method-length 29) "G"
-		   (<= 30 method-length 39) "A"
-		   (<= 40 method-length) "B"
-		   :else "C")))
+	(cond
+		(nil? method-length) "C"
+	   	(<= 1 method-length 5) "C"
+	   	(<= 6 method-length 10) "D"
+	   	(<= 11 method-length 15) "E"
+	   	(<= 16 method-length 20) "F"
+	   	(<= 21 method-length 29) "G"
+	   	(<= 30 method-length 39) "A"
+	   	(<= 40 method-length) "B"
+	   	:else "C"))
 
 (defn pick-octave-for-line-length [line-length]
 	(cond
-	   (<= 10 line-length 39) octave-1
-	   (<= 40 line-length 79) octave-2
-	   (<= 80 line-length 99) octave-3
-	   (<= 100 line-length 119) octave-4
-	   (<= 120 line-length) octave-5))
+	   	(<= 10 line-length 39) octave-1
+	   	(<= 40 line-length 79) octave-2
+	   	(<= 80 line-length 99) octave-3
+	   	(<= 100 line-length 119) octave-4
+	   	(<= 120 line-length) octave-5))
