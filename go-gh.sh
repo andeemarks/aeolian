741da0ca4c36f97fb8373cf005d077940235d06c
Andy Marks <amarks@thoughtworks.com>
2017-04-27 09:50:05 +1000

    Less logging - more relevant

--- go-gh.sh
+++ go-gh.sh
@@ -5,19 +5,39 @@ if [[ $# -lt 2 ]]; then
     exit 1
 fi
 
+# check deps
+if ! which abc2midi &> /dev/null; then
+	>&2 echo “Error: Please install abc2midi and make sure it is in your path”
+	exit 1
+fi
+
+if ! which lein &> /dev/null; then
+	>&2 echo “Error: Please install Leinginen and make sure it is in your path”
+	exit 1
+fi
+
+if ! which java &> /dev/null; then
+	>&2 echo “Error: Please install Java and make sure it is in your path”
+	exit 1
+fi
+
+if ! which timidity &> /dev/null; then
+	>&2 echo “Error: Please install timidity and make sure it is in your path”
+	exit 1
+fi
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 GITHUB_USER=$1
 GITHUB_REPO=$2
 
 echo -e "\e[33mChecking GitHub for repo '$GITHUB_REPO' for user '$GITHUB_USER'...\e[0m"
-curl "https://api.github.com/repos/$GITHUB_USER/$GITHUB_REPO/languages"
 
 WORK_DIR=`mktemp -d -p "$DIR"`
 BIN_DIR=$DIR
 
+# Get code
 echo -e "\e[33mCloning repo to $WORK_DIR...\e[0m"
 cd $WORK_DIR
-
 git clone -q https://github.com/$GITHUB_USER/$GITHUB_REPO.git
 
 cd $BIN_DIR
@@ -25,17 +45,18 @@ cd $BIN_DIR
 echo -e "\e[33mRunning metrics on all Java files...\e[0m"
 
 # find . -name "*.java" | xargs -t -n1 git blame -f -t -e | awk '{print $1 " " $2 " " $3 " " $6}' > blames.txt
+# Run each non Test source file through go.sh
 find $WORK_DIR -regextype sed -regex ".*[^Test]\.java" -exec ./go.sh '{}' $WORK_DIR \;
 
 UBERMETRICSFILE=${WORK_DIR}/${GITHUB_REPO}.metrics
-echo -e "\033[33mBuilding Uber metrics file...\033[0m"
+echo -e "\033[33mBuilding uber metrics file...\033[0m"
 cat $WORK_DIR/*.metrics.all > ${UBERMETRICSFILE}
 
 echo -e "\033[33mGenerating ABC notation...\033[0m"
 lein run ${UBERMETRICSFILE}
 
 echo -e "\033[33mGenerating MIDI...\033[0m"
-abc2midi ${UBERMETRICSFILE}.abc -o ${UBERMETRICSFILE}.mid
+abc2midi ${UBERMETRICSFILE}.abc -s -o ${UBERMETRICSFILE}.mid
 
 echo -e "\033[33mPlaying All MIDI...\033[0m"
 timidity ${UBERMETRICSFILE}.mid
--- go.sh
+++ go.sh
@@ -19,7 +19,7 @@ METHODLENGTHMETRICS=${OUTPUTDIR}/${RAWSOURCECLASSNAME}.method-length.metrics
 INDENTATIONMETRICS=${OUTPUTDIR}/${RAWSOURCECLASSNAME}.indentation.metrics
 
 if [[ -r $SOURCEFILE ]]; then
-	echo -e "\033[1;34mProcessing ${SOURCEFILE} into ${OUTPUTDIR}...\033[0m"
+	echo -e "\033[1;34mProcessing ${RAWSOURCECLASSNAME} into ${OUTPUTDIR}...\033[0m"
 	echo -e "\033[34mGenerating Checkstyle cyclomatic complexity metrics...\033[0m"
 	java -jar $CHECKSTYLEDIR/checkstyle-7.4-all.jar -c $CHECKSTYLEDIR/checkstyle-complexity.xml $SOURCEFILE | grep "[ERROR]" | awk '{print $2 " " $3}' | awk -F: '{{printf "%s#%d CC=%d\n", $1, $2, $4 }}' > ${COMPLEXITYMETRICS}
 	echo -e "\033[34mGenerating Checkstyle line length metrics...\033[0m"
@@ -28,7 +28,7 @@ if [[ -r $SOURCEFILE ]]; then
 	java -jar $CHECKSTYLEDIR/checkstyle-7.4-all.jar -c $CHECKSTYLEDIR/checkstyle-methodlength.xml $SOURCEFILE | grep "[ERROR]" | awk '{print $2 " " $3}' | awk -F: '{printf "%s#%d ML=%d\n", $1, $2, $4 }' > ${METHODLENGTHMETRICS}
 	echo -e "\033[34mGenerating Checkstyle indentation metrics...\033[0m"
 	java -jar $CHECKSTYLEDIR/checkstyle-7.4-all.jar -c $CHECKSTYLEDIR/checkstyle-indentation.xml $SOURCEFILE | grep "[ERROR]" | awk '{print $2 " " $3}' | awk -F: '{printf "%s#%d IND\n", $1, $2 }' > ${INDENTATIONMETRICS}
-	echo -e "\033[34mMerging all metrics...\033[0m"
+	# echo -e "\033[34mMerging all metrics...\033[0m"
 	join -a 1 <(sort -k 1b,1 ${LINELENGTHMETRICS}) <(sort -k 1b,1 ${COMPLEXITYMETRICS}) > ${COMBINEDMETRICSFILE}.tmp
 	join -a 1 <(sort -k 1b,1 ${COMBINEDMETRICSFILE}.tmp) <(sort -k 1b,1 ${INDENTATIONMETRICS}) | sort -V > ${COMBINEDMETRICSFILE}.tmp2
 	join -a 1 <(sort -k 1b,1 ${COMBINEDMETRICSFILE}.tmp2) <(sort -k 1b,1 ${METHODLENGTHMETRICS}) | sort -V > ${COMBINEDMETRICSFILE}
