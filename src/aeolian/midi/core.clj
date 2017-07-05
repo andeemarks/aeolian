70744fbd9f80ab5f457ec47ddb1685d950274084
Andy Marks <amarks@thoughtworks.com>
2017-07-05 17:19:55 +1000

    Updated instrument list

--- src/aeolian/midi/core.clj
+++ src/aeolian/midi/core.clj
@@ -10,15 +10,17 @@
 (def first-beat-emphasis "%%MIDI beatstring pppp\n")
 (def header "%%MIDI gchord c\n")
 
+(def piano 4)
+(def marimba 13)
+(def bass 35)
 (def flute 73)
-(def voice 53)
 (def tuba 58)
 (def trumpet 56)
 (def cello 42)
 (def guitar 26)
 (def accordian 21)
 
-(def instruments [flute voice tuba trumpet cello guitar accordian])
+(def instruments [piano marimba bass flute tuba trumpet cello guitar accordian])
 
 (defn instrument-for [filename]
   (nth instruments
