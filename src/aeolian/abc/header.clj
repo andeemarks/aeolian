b190b834ceb62fa7615128eb63a3f43407ab1fc4
Andy Marks <vampwillow@gmail.com>
2017-08-24 14:10:52 +1000

    Re-introduced jazz drum accompanyment

--- src/aeolian/abc/header.clj
+++ src/aeolian/abc/header.clj
@@ -8,9 +8,11 @@
 (def xxx-major-key "\nK:C")
 (def xxx-minor-key "\nK:Amin")
 (def note-length "\nL:1/8")
+(def misc "\n%%MIDI chordvol 50")
+(def drums "\n%%MIDI channel 1\n%%MIDI drum dd2d2ddd2d2d 59 51 51 59 59 51 51 59 100 50 50 100 100 50 50 100\n%%MIDI drumon")
 
 (defn build-common-header [title key]
-  (str reference title-prefix title composer meter note-length "\n" t/starting-tempo key))
+  (str reference title-prefix title composer meter note-length "\n" t/starting-tempo key misc drums))
 
 (defn build-major-header [title]
   (build-common-header title xxx-major-key))
